5bc6c4d9c1ea89444ff2bcf0583434915c668b1a
mercedes sandu <mxrcedes@gmail.com>
2023-05-06 18:16:41 -0500

    rename game master to level manager

--- Assets/Code/Scripts/LevelManager.cs
+++ Assets/Code/Scripts/LevelManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Code.Scripts
 {
-    public class ProgressionSaver : MonoBehaviour
+    public class LevelManager : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
