6b6be13f2ae66973bd89ebcff4a479ad9f79cfa1
mercedes sandu <mxrcedes@gmail.com>
2023-05-25 02:11:07 -0500

    add some ui icons

--- Assets/Code/Scripts/InGameUI.cs
+++ Assets/Code/Scripts/InGameUI.cs
@@ -1,19 +1,42 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Code.Scripts
 {
     public class InGameUI : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField] private TextMeshProUGUI numPlasticsText;
+        [SerializeField] private TextMeshProUGUI plasticPointsText;
+        [SerializeField] private Canvas pauseMenuCanvas;
+        [SerializeField] private GameObject pauseButton;
         
+        private SpriteRenderer _pauseButtonSpriteRenderer;
+        private Button _pauseButton;
+        private bool _isPaused = false;
+
+        private void Awake()
+        {
+            _pauseButtonSpriteRenderer = pauseButton.GetComponent<SpriteRenderer>();
+            _pauseButton = pauseButton.GetComponent<Button>();
+            pauseMenuCanvas.enabled = false;
+            numPlasticsText.text = $"x {ProgressionSaver.Instance.playerPlasticsCollected}";
+            plasticPointsText.text = $"x {ProgressionSaver.Instance.playerPlasticPoints}";
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
+            
+        }
         
+        public void PauseButton()
+        {
+            
+        }
+
+        public void ResumeButton()
+        {
+            
         }
     }
 }
